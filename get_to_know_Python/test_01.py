4f069c4ca368460a7ed4107479bc63a473090fb1
Weizhe Li <weizheli@gmail.com>
2019-12-07 11:08:52 -0500

    Add files via upload

---
+++ get_to_know_Python/test_01.py
@@ -0,0 +1,214 @@
+import numpy as np 
+msg = 'Hello, python'
+print(msg)
+msg.capitalize()
+10+20
+30
+5*4
+print('hahhaha')
+print('hahhaha')
+a = 'why?python! i hate you !!!!~````~ buy!!! this!!! today!!!'
+print(a)
+input()
+a = input()
+a
+b = input()
+e=domeass
+b
+a+b
+c = input()
+a+b+c
+d = input()
+d
+a+d+b+d+c
+d = input()
+a+d+b+d+c
+z=input()
+z
+z+a+b+c+d
+m=input()
+z+b+c+d+e
+e=input()
+len(e)
+e.capitalize()
+e.upper()
+p=input()
+a+b+c+p+n+z
+10+20
+p=input
+
+for letter in a:
+    print(letter)
+b
+for letter in b:
+    print(letter)
+b="spell'yup\n"
+b = "spell'yup'"
+a.upper()
+a.lower()
+def andrew_cal(num):
+    num*1000
+    return num*1000
+
+andrew_cal(4)
+andrew_cal(1000)
+print('your favorite pet is %s' %s (pet))
+def love_andrew(pet):
+    print('your favorite pet is %s' %s (pet))
+
+love_andrew(lizard)
+love_andrew('lizard')
+def love_andrew(pet):
+    print('your favorite pet is %s' % pet)
+
+love_andrew('lizard')
+def love_andrew(pet):
+    print('your favorite pet is a %s' % pet)
+    a.upper()
+
+def love_andrew(a):
+    print('your favorite pet is a %s' % a.upper())
+
+def love_andrew():
+    a = input('What is your favorite animal?')
+    print('your favorite pet is a %s' % a.upper())
+
+love_andrew()
+a
+a[0:13]
+a[0:14]
+m = 'i hate you becuase bla  bla bla >..'
+m+a[0:8]
+m+a[0:11]
+m[0:10]+a[0:11]
+###################################### Turtle Project Introduction ##############
+import turtle
+tim=turtle.Turtle()
+print('andrew')
+print('andrew\nsheryl')
+animal = input('what is your favorite anmial?')
+animal = input('what is your favorite anmial?')
+animal.upper()
+print('your favorite animal is %s' %animal)
+print('your favorite animal is a %s'%animal)
+def andrew_func():
+    animal = input('what is your favorite anmial?')
+    print('your favorite animal is a %s'%animal')
+    print('your favorite animal is %s' %animal')
+
+def andrew_func():
+    animal = input('what is your favorite anmial?')
+    print('your favorite animal is %s' %animal)
+    
+andrew_func()
+#################################### Learn to do Turtle ####################
+import turtle
+tim=turtle.Turtle()
+tim.color('red')
+tim.pensize(5)
+tim.shape()
+'classic'
+tim.shape('turtle')
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.right(90)
+tim.color('green')
+tim.forward(100)
+tim.penup()
+tim.forward(100)
+tim.right(90)
+tim.pendown()
+tim.color('green')
+tim.forward(100)
+dave=turtle.Turtle()
+dave.color('blue')
+dave.pensize(20)
+dave.backward(100)
+dave.speed(1)
+import turtle
+import random
+tim=turtle.Turtle()
+colors=('red'.'blue'.'green'.'purple'.'yellow'.'orange'.'black'}
+#################################### Turtle First Try ########################
+>>> import turtle
+>>> import random
+>>> tim=turtle.Turtle()
+>>> colors=['red'],['blue'],['green'],['purple'],['yellow'],['orange'],['black']
+>>> tim.color('red','blue')
+>>> tim.width(5)
+>>> tim.begin_fill()
+>>> tim.circle(50)
+>>> tim_fill()
+>>> tim.end_fill()
+>>> tim.penup()
+>>> tim.forward(150)
+>>> tim.pendown()
+>>> tim.color('yellow','black')
+>>> tim.begin_fill()
+>>> for x in range(4):
+...     tim.forward(100)
+...     tim.right(90)
+...
+>>> tim.end_fill()
+>>> tim.set_pos(-200)
+>>> for x in range(5):
+...     randColor=random.randrange(0, len(colors))
+...     tim.color(colors[randColor],colors[random.randrange(0,len(colors))])
+...     rand1=random.randrange(-300,300)
+...     rand2=random.randrange(-300,300)
+...     tim.penup()
+...     tim.setpos((rand1, rand2))
+...     tim.pendown()
+...     tim.begin_fill()
+...     tim.circle(random.randrange(0,80))
+...     tim.end_fill()
+#################################### Turtle Project ###########################
+import turtle
+import random
+
+tim = turtle.Turtle()
+tim.speed(0)
+tim.width(5)
+colors=['red','blue','green','purple','yellow','orange','black']
+def up():
+     tim.setheading(90)
+     tim.forward(100)
+
+def down():
+     tim.setheading(270)
+     tim.forward(100)
+
+def left():
+     tim.setheading(180)
+     tim.forward(100)
+
+def right():
+     tim.setheading(0)
+     tim.forward(100)
+
+turtle.listen()
+turtle.onkey(left, 'Left')
+turtle.onkey(right,'Right')
+turtle.mainloop()
+
+############################### Turtle Function ###############
+import turtle
+import random
+tim=turtle.Turtle()
+colors=['red','blue','green','purple','yellow','orange','black']
+ def andrew_draw(x):
+     for n in range(x):
+             randColor = random.randrange(0, len(colors))
+             tim.color(colors[randColor], colors[random.randrange(0, len(colors))])
+             rand1=random.randrange(-300, 300)
+             rand2=random.randrange(-300, 300)
+             tim.penup()
+             randColor=random.randrange(0, len(colors))
+             tim.setpos((rand1, rand2))
+             tim.pendown()
+             tim.begin_fill()
+             tim.circle(random.randrange(0, 80))
+             tim.end_fill()
+
+andrew_draw(1000000)
